1c052715bd4cd07c6d8e25706601bc091afb641c
Joni4Games <joni.f2003@gmail.com>
2017-06-07 09:30:29 +0200

    Add files via upload

---
+++ resources/app/main.js
@@ -0,0 +1,98 @@
+const {app, BrowserWindow} = require('electron')
+const electron = require('electron');
+const path = require('path')
+const url = require('url')
+const remote = require('electron').remote
+var ipc = require('electron').ipcMain
+const shell = require('electron').shell
+const menu = require('electron')
+const Menu = electron.Menu;
+var closechecked = false;
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+	  width: 800,
+	  height: 600,
+	  title: "GKeep"
+	  })
+    const menuTemplate = [
+        {
+            label: 'App',
+			submenu: [
+				{
+					label: 'Quit',
+					click: () => {
+						var closechecked = true;
+						win.close();
+					}
+				}
+			]
+
+        }
+    ];
+    const menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
+win.setIcon('resources/icon.png');
+  // and load the webpage.
+win.loadURL(url.format({
+    pathname: 'keep.google.com/u/0/',
+    protocol: 'https:',
+    slashes: true
+}))
+win.webContents.on('did-finish-load',() => {
+    win.setTitle("Lightbulb");
+})
+//const {dialog} = require('electron')
+//dialog.showErrorBox("Hey!", "Dies ist eine Fehlermeldung!")
+
+  //// Open the DevTools.
+  //win.webContents.openDevTools()
+  // Emitted when the window is closed.
+//win.on('closed', (event) => {
+//    // Dereference the window object, usually you would store windows
+//    // in an array if your app supports multi windows, this is the time
+//
+//    event.preventDefault();
+//
+//
+//	if (win.isDestroyed()) {
+//
+//			createWindow();
+//			win.minimize();
+//
+//	}
+//	})
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+//app.on('window-all-closed', () => {
+//  // On macOS it is common for applications and their menu bar
+//  // to stay active until the user quits explicitly with Cmd + Q
+//  if (process.platform !== 'darwin') {
+//    app.quit()
+//  }
+
+app.on('window-all-closed', app.quit);
+app.on('before-quit', () => {
+    mainWindow.removeAllListeners('close');
+    mainWindow.close();
+});
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+		win.quit();
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
